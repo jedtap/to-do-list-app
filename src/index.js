11d7f487e7631e7cd52bcbf42ae1042d18b7a9f6
Jed-Tapiador <doom.slaver2@gmail.com>
2022-06-22 20:00:13 +0800

    re organize the JS code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
 import 'bootstrap';
 
-// -- Modal visibility -- 
+// -- Functions -- 
+
+// Modal visibility
 
 function openModal(section, index){
   closeModal();
@@ -15,12 +17,10 @@ function openModal(section, index){
       editNameModal.style.display = "block";
     break;
     case "task":
-
       viewTitle.value = projectsLibrary[index].todos.title;
       viewDue.value = projectsLibrary[index].todos.due;
       viewDesc.textContent = projectsLibrary[index].todos.desc;
       saveChanges.setAttribute("data-index",index);
-
       viewTask.style.display = "block";
     break;
     case "new-project":
@@ -43,17 +43,48 @@ function closeModal(){
   closeProjects.style.display = "inline";
 }
 
-// -- Home functions --
+// Internal processing functions
+
+function projects(name, title, due, desc, prio) {
+  this.name = name;
+  this.todos = {
+      title:title,
+      due:due,
+      desc:desc,
+      prio:prio
+  };
+};
+
+function saveToMemory(){
+  memory.clear();
+  memory.setItem("projectsLibrary", JSON.stringify(projectsLibrary));
+}
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+// Home functions
+
+function generatePage(index){
+  updateProjectText(listOfProjects[index]);
+  clearPage();
+  for(var x in projectsLibrary){
+    if(projectsLibrary[x]){ 
+      if (listOfProjects[index] == projectsLibrary[x].name) { displayToDos(projectsLibrary[x], x) };
+    }
+  }
+  addTaskButtons();
+}
 
 function sort(order){
   switch(order){
     case "priority":
       priority.style.display = "none";
       dateAdded.style.display = "inline";
-
       clearPage();
-
       dummyLibrary = [];
+
       for(var x in projectsLibrary){
         if(projectText.textContent == projectsLibrary[x].name && projectsLibrary[x].todos.prio){
           displayToDos(projectsLibrary[x], x);
@@ -66,13 +97,12 @@ function sort(order){
           dummyLibrary.push(projectsLibrary[x]);
         };      
       }
-      addTaskButtons();
 
+      addTaskButtons();
     break;
     case "date":
       priority.style.display = "inline";
       dateAdded.style.display = "none";
-
       clearPage();
 
       for(var x in projectsLibrary){
@@ -80,12 +110,26 @@ function sort(order){
           if (projectText.textContent == projectsLibrary[x].name) { displayToDos(projectsLibrary[x], x) };
         }
       }
+
       addTaskButtons();
-
-
     break;
-
   }
+}
+
+function addTaskButtons(){
+  prio = document.querySelectorAll(".prio");
+  task = document.querySelectorAll(".task");
+  trashTask = document.querySelectorAll(".delete-task");
+
+  prio.forEach(prio => {
+    prio.addEventListener("click", () => prioritize(prio.textContent, prio.dataset.index));
+  });
+  task.forEach(task => {
+    task.addEventListener("click", () => openModal("task", task.dataset.index));
+  });
+  trashTask.forEach(trash => {
+    trash.addEventListener("click", () => deleteTask(trash.dataset.index));
+  });
 }
 
 function prioritize(status, index){
@@ -101,8 +145,31 @@ function prioritize(status, index){
   saveToMemory();
 }
 
+function saveTaskChanges(){
+  index = saveChanges.dataset.index;
 
-//  -- Project maintenance --
+  projectsLibrary[index].todos.title = viewTitle.value;
+  projectsLibrary[index].todos.due = viewDue.value;
+  projectsLibrary[index].todos.desc = viewDesc.value;
+
+  revisedTitle = document.querySelector(`[data-index="${index}"][class="card-title"]`);
+  revisedDue = document.querySelector(`[data-index="${index}"][class="card-subtitle mb-2 text-muted"]`);
+
+  revisedTitle.textContent = viewTitle.value;
+  revisedDue.textContent = viewDue.value;
+
+  saveToMemory();
+  closeModal();
+}
+
+function deleteTask(index){
+  selectedTrashTask = document.querySelector(`[data-index="${index}"][class="card"]`);
+  selectedTrashTask.style.display = "none";
+  projectsLibrary[index] = false;
+  saveToMemory();
+}
+
+//  Project maintenance
 
 function deleteProject(index){
   let ans = confirm("Warning! Are you sure to delete the project with all of its tasks?");
@@ -113,7 +180,6 @@ function deleteProject(index){
     for(var x in projectsLibrary){
       if(listOfProjects[index] == projectsLibrary[x].name){ projectsLibrary[x] = false };
     }
-
     if(listOfProjects[index] == projectText.textContent){
       projectText.textContent = "";
       clearPage();
@@ -122,24 +188,77 @@ function deleteProject(index){
 
     listOfProjects[index] = false;
     saveToMemory();
-
   };
 }
 
+function createNewProj(){
+  
+  let duplicate = false
+
+  for(var y in listOfProjects){
+    if(listOfProjects[y] == newProjName.value){ duplicate = true };
+  }
+  if(duplicate == false){
+    listOfProjects.push(newProjName.value);
+    updateProjectText(newProjName.value);
+    generateProject(newProjName.value, listOfProjects.length - 1);
+    newProjName.value = "";
+  }
+  clearPage();
+  closeModal();
+}
+
+function saveNewProjName(index){
+  oldProjName = listOfProjects[index];
+
+  if(projectText.textContent == oldProjName){
+    projectText.textContent = editProjName.value;
+  }
+
+  listOfProjects[index] = editProjName.value;
 
 
+  for(var x in projectsLibrary){
+    if(projectsLibrary[x].name == oldProjName){ projectsLibrary[x].name = editProjName.value }
+  }
 
-// -- Uncategorized --
+  clearProjList();
+  for(var y in listOfProjects){ generateProject(listOfProjects[y], y) };
+  saveToMemory();
+}
 
-function projects(name, title, due, desc, prio) {
-  this.name = name;
-  this.todos = {
-      title:title,
-      due:due,
-      desc:desc,
-      prio:prio
-  };
-};
+// New task modal
+
+function saveTheNewTask(){
+  let newTask = new projects(projectText.textContent, newTitle.value, newDue.value, newDesc.value, false);
+  projectsLibrary.push(newTask);
+  saveToMemory();
+  displayToDos(newTask, projectsLibrary.length - 1 );
+  addTaskButtons();
+
+  newTitle.value = "";
+  newDue.value = "";
+  newDesc.value = "";
+
+  closeModal();
+  saveToMemory();
+}
+
+// -- DOM Related --
+
+// Home
+
+function updateProjectText(title){
+  projectText.textContent = title;
+}
+
+function clearPage(){
+  var child = content.lastElementChild; 
+  while (child) {
+    content.removeChild(child);
+    child = content.lastElementChild;
+  }
+}
 
 function displayToDos(project, index){
   // Goal is to display this HTML structure to class .content
@@ -188,39 +307,7 @@ function displayToDos(project, index){
   content.appendChild(dcard);
 }
 
-function saveToMemory(){
-  memory.clear();
-  memory.setItem("projectsLibrary", JSON.stringify(projectsLibrary));
-}
-
-function updateProjectText(title){
-  projectText.textContent = title;
-}
-
-
-function saveTheNewTask(){
-  let newTask = new projects(projectText.textContent, newTitle.value, newDue.value, newDesc.value, false);
-  projectsLibrary.push(newTask);
-  saveToMemory();
-
-  displayToDos(newTask, projectsLibrary.length - 1 );
-  addTaskButtons();
-
-  newTitle.value = "";
-  newDue.value = "";
-  newDesc.value = "";
-
-  closeModal();
-  saveToMemory();
-}
-
-function clearPage(){
-  var child = content.lastElementChild; 
-  while (child) {
-    content.removeChild(child);
-    child = content.lastElementChild;
-  }
-}
+// Projects Modal
 
 function clearProjList(){
   var child = listDiv.lastElementChild; 
@@ -228,51 +315,6 @@ function clearProjList(){
     listDiv.removeChild(child);
     child = listDiv.lastElementChild;
   }
-}
-
-
-function addTaskButtons(){
-  prio = document.querySelectorAll(".prio");
-  task = document.querySelectorAll(".task");
-  trashTask = document.querySelectorAll(".delete-task");
-
-  prio.forEach(prio => {
-    prio.addEventListener("click", () => prioritize(prio.textContent, prio.dataset.index));
-  });
-  task.forEach(task => {
-    task.addEventListener("click", () => openModal("task", task.dataset.index));
-  });
-  trashTask.forEach(trash => {
-    trash.addEventListener("click", () => deleteTask(trash.dataset.index));
-  });
-}
-
-function deleteTask(index){
-  selectedTrashTask = document.querySelector(`[data-index="${index}"][class="card"]`);
-  selectedTrashTask.style.display = "none";
-  projectsLibrary[index] = false;
-  saveToMemory();
-}
-
-function saveTaskChanges(){
-  index = saveChanges.dataset.index;
-
-  projectsLibrary[index].todos.title = viewTitle.value;
-  projectsLibrary[index].todos.due = viewDue.value;
-  projectsLibrary[index].todos.desc = viewDesc.value;
-
-  revisedTitle = document.querySelector(`[data-index="${index}"][class="card-title"]`);
-  revisedDue = document.querySelector(`[data-index="${index}"][class="card-subtitle mb-2 text-muted"]`);
-
-  revisedTitle.textContent = viewTitle.value;
-  revisedDue.textContent = viewDue.value;
-
-  saveToMemory();
-  closeModal();
-}
-
-function onlyUnique(value, index, self) {
-  return self.indexOf(value) === index;
 }
 
 function generateProject(project, index){
@@ -319,58 +361,6 @@ function generateProject(project, index){
   listDiv.appendChild(ditem);
 }
 
-
-function saveNewProjName(index){
-
-  // editProjName.value = listOfProjects[index];
-
-  oldProjName = listOfProjects[index];
-
-  if(projectText.textContent == oldProjName){
-    projectText.textContent = editProjName.value;
-  }
-
-  listOfProjects[index] = editProjName.value;
-
-
-  for(var x in projectsLibrary){
-    if(projectsLibrary[x].name == oldProjName){ projectsLibrary[x].name = editProjName.value }
-  }
-
-  clearProjList();
-  for(var y in listOfProjects){ generateProject(listOfProjects[y], y) };
-  saveToMemory();
-}
-
-function generatePage(index){
-  updateProjectText(listOfProjects[index]);
-  clearPage();
-  for(var x in projectsLibrary){
-    if(projectsLibrary[x]){ 
-      if (listOfProjects[index] == projectsLibrary[x].name) { displayToDos(projectsLibrary[x], x) };
-    }
-  }
-  addTaskButtons();
-}
-
-
-function createNewProj(){
-  
-  let duplicate = false
-
-  for(var y in listOfProjects){
-    if(listOfProjects[y] == newProjName.value){ duplicate = true };
-  }
-  if(duplicate == false){
-    listOfProjects.push(newProjName.value);
-    updateProjectText(newProjName.value);
-    generateProject(newProjName.value, listOfProjects.length - 1);
-    newProjName.value = "";
-  }
-  clearPage();
-  closeModal();
-}
-
 // -- Initial variables --
 
 // Modal
@@ -386,56 +376,63 @@ const projectText = document.querySelector("h2");
 const content = document.querySelector(".content");
 
 // Header buttons
-const projectsButton = document.querySelector(".projects-button");
 const newTask = document.querySelector(".new-task");
 const priority = document.querySelector(".priority");
 const dateAdded = document.querySelector(".date-added");
+const projectsButton = document.querySelector(".projects-button");
 
-// Task buttons
-let prio;
-let task;
-let trashTask;
+// Variables used when creating the DOM of each task
+let dcard, diconPrio, dtask, h4, h5, diconDeleteTask;
+
+// Variables used to add event listeners to each task card
+let prio, task, trashTask;
+
+// Selection varaibles to know which task card prio and delete was selected
+let selectedPrio, selectedTrashTask;
+
+// Selection varaibles to know what is the new value for the task title and due
+let revisedTitle, revisedDue;
 
 // List of projects
-let pencil = document.querySelectorAll(".pencil");
-let projectTitle = document.querySelectorAll(".project-title");
-let trashProject = document.querySelectorAll(".delete-project");
 const closeProjects = document.querySelector(".close-projects");
 const newProjectButton = document.querySelector(".new-project-button");
 const listDiv = document.querySelector(".list");
 
 // Edit project name
+const editProjName = document.querySelector(".edit-proj-name");
 const saveName = document.querySelector(".save-name");
 
-// Creating a totally new project
+// Totally new project
+let newProjName = document.querySelector(".new-proj-name");
+
+
+// Deleting project name.. or the whole project
+let oldProjName, deleteProjItem;
+
+// Creating a totally new project name
 const createProject = document.querySelector(".create-project");
 
-// Viewing/Editing/Creating a task
+// Variables when creating a totally new task via the modal
 const newTitle = document.querySelector(".new-title"); 
 const newDue = document.querySelector(".new-due");
 const newDesc = document.querySelector(".new-desc");
-const closeTask = document.querySelectorAll(".close-task");
-const saveChanges = document.querySelector(".save-changes");
 const saveNewTask = document.querySelector(".save-new-task");
+
+// Viewing/Editing a task
 const viewTitle = document.querySelector(".view-title");
 const viewDue = document.querySelector(".view-due");
 const viewDesc = document.querySelector(".view-desc");
+const saveChanges = document.querySelector(".save-changes");
 
+// Avaiable for both task modals
+const closeTask = document.querySelectorAll(".close-task");
 
-// -- Uncategorized --
-let dcard, diconPrio, dtask, h4, h5, diconDeleteTask;
-let memory = window.localStorage;
-let dummyLibrary = [];
-let selectedPrio, selectedTrashTask;
-let revisedTitle, revisedDue;
-let index;
+// Memory related
+let projectsLibrary = [];
 let listOfProjects = [];
-let ditem, diconPencil, dprojTitle, diconDeleteProj;
-const editProjName = document.querySelector(".edit-proj-name");
-let oldProjName, revisingProjName ;
-let deleteProjItem;
-let newProjName = document.querySelector(".new-proj-name");
-
+let dummyLibrary = [];
+let index;
+let memory = window.localStorage;
 
 // -- Initial event listeners --
 
@@ -444,8 +441,6 @@ projectsButton.addEventListener("click", () => openModal("projects"));
 newTask.addEventListener("click", () => openModal("new-task"));
 priority.addEventListener("click", () => sort("priority"));
 dateAdded.addEventListener("click", () => sort("date"));
-
-
 
 // List of projects
 closeProjects.addEventListener("click", () => closeModal());
@@ -467,21 +462,15 @@ closeTask.forEach(closeTask => {
 saveChanges.addEventListener("click", () => saveTaskChanges());
 saveNewTask.addEventListener("click", () => saveTheNewTask());
 
+// -- Have a demo? Use this initial data --
 
-
-
-
-// -- Populate initial data --
-
-let projectsLibrary = [];
 projectsLibrary.push(new projects("Dive to ocean floor","Create ship","2022-07-22","Lorem hwiL wihdkwleW", false));
 projectsLibrary.push(new projects("Dive to ocean floor","Find boat","2022-07-30","Lorem hwiL wihdkwleW", true));
 projectsLibrary.push(new projects("Drill to the earth core","Find team magma","2022-08-15","Lorem hwiL wihdkwleW", false));
 saveToMemory();
 
+// -- Retreive projectsLibrary from memory and display first project --
 
-
-// Retreive projectsLibrary from memory and display first project
 if (memory.getItem("projectsLibrary")){
   projectsLibrary = JSON.parse(memory.getItem("projectsLibrary"));
 
@@ -500,9 +489,6 @@ if (memory.getItem("projectsLibrary")){
   }
 
   listOfProjects = listOfProjects.filter(onlyUnique);
-
-
   for(var y in listOfProjects){ generateProject(listOfProjects[y], y) };
-
   addTaskButtons();
 }
